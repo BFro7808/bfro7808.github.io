afa65b957c598744cde32b551055d97d8a7b4934
BFro7808 <113703235+BFro7808@users.noreply.github.com>
2024-05-07 23:27:42 -0500

    Combined a lot of script files

--- tofuGame/Data/effects.js
+++ tofuGame/Data/effects.js
@@ -1,3 +1,108 @@
+var power=0
+const POWER_MAX = 100
+
+class blast{
+    constructor(position){
+        this.position = position
+        this.radius=40
+        this.id = this.getRandInt(1000000, 9999999)
+
+        this.hasDonePulse=false
+
+        this.innerAlpha=0.3
+
+        gameSound.playSound(13)
+    }
+
+    update(){
+        this.checkInRadius() //Test and remove bullets inside of the blast radius
+
+        if(this.hasDonePulse==false){
+            this.position = this.getPlayerPos()
+            this.radius-=5
+            if(this.radius<=0){
+                this.radius=0
+                setTimeout(() => {
+                    this.hasDonePulse = true
+                }, 500);
+            }
+        }
+        else{
+            this.innerAlpha-=0.0025
+            this.radius += 10
+            if (this.radius >= 1000){
+                this.removeMyself(blastArray)
+                if(blastArray.length==0){
+                    playerObject.invul=false
+                    playerObject.alpha=1
+                }
+            } 
+        }
+    }
+
+    draw(c){
+
+        c.beginPath()
+        c.arc(this.position[0], this.position[1], this.radius, 0, 2 * Math.PI)
+        c.strokeStyle = "white"
+        c.lineWidth = 7
+        c.fillStyle = "yellow"
+
+        c.globalAlpha = this.innerAlpha
+        c.fill()
+        c.globalAlpha = 1
+        c.stroke()
+    }
+
+    checkInRadius(){
+        for (let i = 0; i < enemyBulletsArray.length;i++){
+            if (this.getDistanceTo(enemyBulletsArray[i])<this.radius*1.25){    //If distance to bullet is <= the radius of this blast
+                enemyBulletsArray[i].removeMyself(enemyBulletsArray)
+                gameSound.playSound(6)
+            }
+        }
+
+        for (let i = 0; i < enemyArray.length;i++){
+            if (this.getDistanceTo(enemyArray[i]) < this.radius * 1.25) {    //If distance to enemy is <= the radius of this blast
+                //force the enemy to move upward 100 pixels
+                let dPos = enemyArray[i].desiredPosition
+                enemyArray[i].desiredPosition = [dPos[0],dPos[1]-4]
+            }
+        }
+    }
+
+    getDistanceTo(object) {
+        let xDiff = Math.abs(this.position[0] - object.position[0])
+        let yDiff = Math.abs(this.position[1] - object.position[1])
+
+        return xDiff + yDiff
+    }
+    
+    getRandInt(min, max) { return Math.trunc(Math.random() * max - min) + min }  //really small random integer function
+
+    removeMyself(arrayToCheck) { //Remove THIS object (checks to see where THIS object is in the given array and removes only the exact match)
+        for (let i = 0; i < arrayToCheck.length; i++) {
+            if (this.equals(arrayToCheck[i])) arrayToCheck.splice(i, 1)
+        }
+    }
+
+    equals(object) { //Are these the same?
+        return this.id == object.id
+    }
+
+    getPlayerPos(){
+        return [playerObject.position[0],playerObject.position[1]]
+    }
+}
+
+
+
+
+
+
+
+
+
 let collideAreaMod = 3
 
 class textPop{
--- tofuGame/Data/player.js
+++ tofuGame/Data/player.js
@@ -239,3 +239,88 @@ class player{
         return this.getRandInt(1000000,9999999)
     }
 }
+
+
+
+
+
+
+
+
+
+class playerFollower{
+    constructor(previousFollowersCount){
+        //this will be the things that follows the player and shoots more bullets
+        this.position=playerObject.getPos()
+        this.desiredPosition=[]
+        this.attackReady=true
+        this.speed=7
+
+        this.offsets=[] //Determine where to attatch the follower based on the players position
+        if(previousFollowersCount==0){
+            this.offsets=[-100,-20]
+        }
+        else if(previousFollowersCount==1){
+            this.offsets=[100,-20]
+        }
+        else{
+            this.offsets=[0,70]
+        }
+    }
+
+    update(){
+        let playerPos=playerObject.getPos()
+        this.position=[playerPos[0]+this.offsets[0],playerPos[1]+this.offsets[1]]
+
+        if(playerObject.shiftSpeedMod==1){this.firingHandeler()} //player isn't holding shift to slow
+    }
+
+    draw(c){
+
+        c.globalAlpha=0.7
+        c.beginPath()
+        c.arc(this.position[0],this.position[1],7,0,2*Math.PI)
+
+        c.strokeStyle="purple"
+        c.lineWidth=7
+
+        c.fillStyle="purple"
+        c.fill()
+
+        c.stroke()
+        c.globalAlpha=1
+    }
+
+    firingHandeler(){
+        if(!(playerObject.attacking&&this.attackReady)) return  //If the cooldown is not gone or is told not to attack
+        this.startAttackCoolDown()
+
+        let newBullets=[]
+
+        let id=this.getRandInt(1000000,9999999)
+        newBullets.push(new playerFollowerBullet(this.position[0],this.position[1], 0,-20,id))
+        id=this.getRandInt(1000000,9999999)
+        newBullets.push(new playerFollowerBullet(this.position[0],this.position[1], 1,-20,id))
+        id=this.getRandInt(1000000,9999999) //this could be a function
+        newBullets.push(new playerFollowerBullet(this.position[0],this.position[1], -1,-20,id))
+        
+        this.pushNewBullets(newBullets) //Function that pushes all the newly made bullets to the playerBulletsArray array
+    }
+
+    startAttackCoolDown(){
+        this.attackReady=false
+        setTimeout(() => {
+            this.attackReady=true
+        }, 100);
+    }
+
+    getRandFloat(min,max) {return (Math.random()*max-min)+min}  //really small random float function
+
+    pushNewBullets(arr){
+        for(let i=0;i<arr.length;i++){
+            playerFollowerBulletsArray.push(arr[i])
+        }
+    }
+    
+    getRandInt(min,max) {return Math.trunc(Math.random()*max-min)+min}  //really small random integer function
+}
