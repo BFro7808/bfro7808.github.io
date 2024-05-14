2db2b64f71ed982dc1d7346f74441569b252c95c
BFro7808 <113703235+BFro7808@users.noreply.github.com>
2024-05-13 19:36:36 -0500

    Pizzas can be added to order list
    Form needs work
    Can't currently place orders

--- finalProject/script.js
+++ finalProject/script.js
@@ -1,8 +1,16 @@
 //document.getElementById("pepBtn").addEventListener("click", fillAuto);
 
-let globalToppings=[]; //A list of toppings the user wants for a pizza, resets when pizza is added to order
+let globalToppings=[];  //A list of toppings the user wants for a pizza, resets when pizza is added to order
 
-let pizzas=[]//List of pizzas in the customer's order
+let pizzas=[]           //List of pizzas in the customer's order
+
+let orderPrice=0        //Price of order without tax
+let orderPriceTotal=0   //Price of order with tax
+let pizzaCount=1        //How many pizza's in order - is always one behind actual pizza count, VISUAL DISPLAY ONLY
+
+const mediumPizzaPrice=10
+const smallPizzaPrice=7
+const largePizzaPrice=13
 
 function toggleButtons(btn){
     let top=btn.innerHTML;  //The name of the topping the user is changing, fecthed from the word on the button (very exploitable!)
@@ -30,13 +38,65 @@ function removeTopping(topping){
 }
 
 function pizza(size){
+    count.innerHTML=pizzaCount++;
     //Dont reset toppings, maybe they want two?
     this.toppings = globalToppings;
     this.size = size;
+    /*Pizza Prices:
+    S - $7
+    M - $10
+    L - $13
+    Topping - +0.25
+    Then %10 sale tax*/
+    if     (size=="medium")orderPrice+=mediumPizzaPrice;    //Add price based on pizza size
+    else if(size=="small") orderPrice+=smallPizzaPrice;
+    else                   orderPrice+=largePizzaPrice;
 
-    pizzas.push(this);
+    orderPrice+=0.25*globalToppings.length; //Add toppings to price
+    orderPriceTotal=roundToHundreths(orderPrice+(orderPrice*0.1));    //Calc tax for total price
+
+    pizzas.push(this);  //Add this pizza to the order
     console.table(pizzas);
+    console.warn("$"+orderPrice,"$"+orderPriceTotal)
+
+    updateOrder(this) //Finally, update the order list on screen so the user sees the pizza added
 }
+
+//order object WRITE DESC
+//a
+//a
+function order(){
+    this.contents=pizzas;
+    this.price=orderPrice;
+    this.priceTotal=orderPriceTotal;
+
+    this.name;
+    this.address;
+}
+
+//Refreshes the Your Order list when a new pizza is added
+function updateOrder(pizza){
+    let str="<infoBit><infoLeft>";
+    //First, add the image depending on the pizza size
+    if(pizza.size=="medium")    str+='<img src="img/iconPizzaMedium.png">'
+    else if(pizza.size=="small")str+='<img src="img/iconPizzaSmall.png">'
+    else str+='<img src="img/iconPizzaLarge.png">'
+
+    str+="</infoLeft><infoRight><br><br>"
+    //Reformat word with capFirstLetter()
+    str+=capFirstLetter(pizza.size)+"<br>"
+    //Use getToppingsList(pizza) to get a list of toppings
+    str+=getToppingsList(pizza)
+
+    //Close of bit
+    str+="</infoRight></infoBit><br>"
+    console.log(str)
+    orderInfo.innerHTML+=str;
+}
+/* Each order info bit will look like this:
+Pizza image     Pizza Size
+Pizza image     Toppings, comma delimited
+*/
 
 //Function that is run whenever the page loads
 function auto(){
@@ -48,4 +108,27 @@ function autoYear(){
     let d = new Date;
     let year= d.getFullYear();
     footer.innerHTML="&copy;"+year;
+}
+
+//Returns the number rounded to the hundreths
+function roundToHundreths(num){
+    num=Math.round(num*100)/100;
+    return num;
+}
+
+//Returns a word with the first letter capitalized
+function capFirstLetter(word){
+    return String(word).substring(0,1).toUpperCase()+word.substring(1); //Capitalize the first letter and then reattach the rest of the word and return it
+}
+
+//Returns a list of all the toppings on a pizza in a comma delimited format
+function getToppingsList(pizza){
+    let list="";
+    if(pizza.toppings.length<1) return "No Toppings"
+    for (let i=0;i<pizza.toppings.length;i++){
+        //If were not on the last topping, add a comma with a space
+        if(i<pizza.toppings.length-1) list+=pizza.toppings[i]+", ";
+        else list+=pizza.toppings[i];
+    }
+    return list;
 }
